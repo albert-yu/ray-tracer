ac01d19376bf04396c762adfb09162b17b27ef93
Albert Yu <albert.yu@gmail.com>
2023-04-19 20:07:22 -0400

    Update window title

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ pub fn main() -> Result<(), String> {
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("Ray tracer", 800, 600)
         .position_centered()
         .opengl()
         .build()
