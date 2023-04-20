f70e458ee4ab917156081589efbf1440d7607cff
Albert Yu <albert.yu@gmail.com>
2023-04-19 20:06:14 -0400

    Start with black

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
